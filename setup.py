4a3ac2b5bfcff439083001a4a0990a65ab8c2db4
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2015-04-19 13:43:17 +0300

    Adds MessageMiddleware for django1.6< support

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ def runtests():
         MIDDLEWARE_CLASSES=(
             'django.contrib.sessions.middleware.SessionMiddleware',
             'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
         ),
         ROOT_URLCONF='djangonumerics.urls',
     )
